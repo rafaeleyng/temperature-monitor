818099b9bfb6dee1172d19cefb318cea9ce059af
Rafael Eyng <rafaeleyng@gmail.com>
2017-11-02 23:05:24 -0200

    implement bot with query for temperature range

--- bot/server.js
+++ bot/server.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const moment = require('moment')
+const parseDuration = require('parse-duration')
 
 const { API_URL, TELEGRAM_TOKEN } = process.env
 
@@ -19,11 +20,13 @@ bot.onText(/\/last (.+)/, (msg, match) => {
   const chatId = msg.chat.id
   const sensorId = match[1]
 
+  const sendErrorMessage = () => bot.sendMessage(chatId, `Unable to get information about sensor ${sensorId}`)
+
   axios.get(`${API_URL}/measurements/last/${sensorId}`)
     .then(res => {
       const data = res.data.data
       if (!data) {
-        bot.sendMessage(chatId, `Unable to get information about sensor ${sensorId}`)
+        sendErrorMessage()
         return
       }
 
@@ -42,6 +45,47 @@ on ${date}`,
     })
     .catch(err => {
       console.error(err)
-      bot.sendMessage(chatId, `Unable to get information about sensor ${sensorId}`)
+      sendErrorMessage()
+    })
+})
+
+bot.onText(/\/range (.+)/, (msg, match) => {
+  const chatId = msg.chat.id
+  const query = match[1].split(' ')
+
+  if (!query[0] || !query[1]) {
+    bot.sendMessage(
+      chatId,
+      'Please follow the format: `/range sensorId fromOffset [fromOffset]`. For more information, /help',
+      { parse_mode: 'Markdown' }
+    )
+    return
+  }
+
+  if (!query[2]) {
+    query[2] = '1s'
+  }
+
+  const [ sensorId, fromOffset, toOffset ] = query
+
+  const now = Date.now()
+  const fromEpoch = now - parseDuration(fromOffset)
+  const toEpoch = now - parseDuration(toOffset)
+
+  const sendErrorMessage = () => bot.sendMessage(chatId, `Unable to get information about sensor ${sensorId}`)
+
+  axios.get(`${API_URL}/measurements/range/${sensorId}?from=${fromEpoch}&to=${toEpoch}`)
+    .then(res => {
+      const data = res.data.data
+      if (!data) {
+        sendErrorMessage()
+        return
+      }
+
+      bot.sendMessage(chatId, JSON.stringify(data))
+    })
+    .catch(err => {
+      console.error(err)
+      sendErrorMessage()
     })
 })
